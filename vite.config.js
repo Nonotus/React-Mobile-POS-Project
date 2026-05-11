d71b19eb5c236b027eb1ca5c0cb41e4616671daf
Nonotus <zarkygreg@gmail.com>
2026-05-11 18:05:14 +0800

    fixed pathing error

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React-Mobile-POS-Project/', // 👈 IMPORTANT (must match repo name)
+  base: '/React-Mobile-POS-Project/',
 })
