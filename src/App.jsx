d7a9c0a074b184eabc4a4f9baa40b38e4a088b9d
Nonotus <zarkygreg@gmail.com>
2026-05-11 18:09:37 +0800

    pathing error fix #2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ItemPage from "./pages/ItemPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/React-Mobile-POS-Project/">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/scan" element={<ScanPage />} />
