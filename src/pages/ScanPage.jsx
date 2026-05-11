26ad579a5237d9608e45d81902b9f7bc07f0e281
Nonotus <zarkygreg@gmail.com>
2026-05-11 20:30:43 +0800

    final fix

--- src/pages/ScanPage.jsx
+++ src/pages/ScanPage.jsx
@@ -27,7 +27,7 @@ export default function ScanPage() {
 
           navigate("/item", {
             state: {
-              item: data.name,
+              item: data.item,
               price: data.price,
             },
           });
